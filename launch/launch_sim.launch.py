23f54c1f753731069e4de6e2313d4416eb6a3640
Jaideep Rao Alladi <alladijaideep@gmail.com>
2024-07-19 15:53:24 -0700

    ros2_control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -9,6 +9,9 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+
 
 
 def generate_launch_description():
@@ -36,10 +39,30 @@ def generate_launch_description():
                         output='screen')
 
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[diff_drive_spawner],
+        )
+    )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        delayed_diff_drive_spawner,
+        joint_broad_spawner
     ])
